e6ccd4235958735a9e481139fd55359bb3ef9718
choiyewon <60353237+choiyewon1004@users.noreply.github.com>
2023-10-06 11:36:16 +0900

    Add files via upload

---
+++ src/main/java/com/example/project_navi/ProjectNaviApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_navi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectNaviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectNaviApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/project_navi/ProjectNaviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_navi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectNaviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
